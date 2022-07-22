80ee2e7b48ad4adafb1046ff009edc19257ad1c9
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-07-22 11:26:10 +0300

    CHORE: Change home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,7 @@
 import Layout from 'components/common/layouts';
-import Recomend from "components/pages/home/recomend/recomend";
-import CardGrid from "components/common/cards_grid/cards_grid";
-import Ad from "UI/ads/ad";
-import cards from 'mock/cards.json'
+import Recomend from 'components/pages/home/recomend/recomend';
+import cards from 'mock/n_cards.json';
+import NavBlock from 'components/pages/home/nav-block';
 
 /**
  *
@@ -11,36 +10,21 @@ import cards from 'mock/cards.json'
  * @returns {JSX.Element}
  * @constructor
  */
-export default function Home({cards}) {
+export default function Home({ cards }) {
 	return (
 		<Layout>
 			<div className="home">
-					<Ad img={cards[0].imgAd} reference={cards[0].reference} type={cards[0].large}/>
-				<div className="card__top">
-					<Recomend/>
-					<CardGrid type='row' cards={cards}/>
-				</div>
-
-				<div className="card__middle">
-					<Recomend/>
-					<div className="card__block__wrapper">
-						<Ad img={cards[0].imgAd} reference={cards[0].reference} type={cards[0].vertical}/>
-						<CardGrid type='block' cards={cards}/>
-					</div>
-				</div>
-
-				<div className="card__bottom">
-					<Recomend/>
-					<Ad img={cards[0].imgAd} reference={cards[0].reference} type={cards[0].large}/>
-					<CardGrid type='row' cards={cards}/>
-				</div>
+				<NavBlock />
+				<Recomend cards={cards} />
+				<Recomend cards={cards} />
+				<Recomend cards={cards} />
 			</div>
 		</Layout>
 	);
 }
 export async function getStaticProps() {
-	console.log(cards)
-	return{
-		props: cards,
-	}
+	console.log(cards);
+	return {
+		props: { cards },
+	};
 }
