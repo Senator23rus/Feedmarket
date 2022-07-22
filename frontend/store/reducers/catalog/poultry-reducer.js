51dd719adf3670335f50b28e630709cae285d3d5
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-07-22 10:45:39 +0300

    CHORE: Create create reducer for catalog page

---
+++ frontend/store/reducers/catalog/poultry-reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+	filters: {
+		type: null,
+	},
+};
+export const PoultryReducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
