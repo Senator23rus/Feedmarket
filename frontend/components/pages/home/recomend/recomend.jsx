23d0b23f4f9f90c5ea74f71514a399ed1c01c386
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-07-22 12:07:50 +0300

    FIX: Fix recommend component

--- frontend/components/pages/home/recomend/recomend.jsx
+++ frontend/components/pages/home/recomend/recomend.jsx
@@ -1,6 +1,6 @@
 import classes from 'components/pages/home/recomend/recomend.module.scss';
-import Button from 'UI/button'
-
+import Button from 'UI/button';
+import Card from 'UI/cards/pc/card';
 
 /**
  * @description -Заголовок с кнопкой
@@ -8,14 +8,44 @@ import Button from 'UI/button'
  * @returns {JSX.Element}
  * @constructor
  */
- const Recomend = () => {
-    return  (
-            <div className={classes.recomend}>
-                <h1 className={classes.recomend__title}>
-                    FEED MARKET рекомендует
-                </h1>
-                <Button factor='yellow' size='s' className={classes.recomend__btn}>Перейти в рекомендации</Button>
-            </div>
-    )
-}
+const Recomend = ({ ad, cards }) => {
+	return (
+		<div className={classes.recomend}>
+			<div className={classes.recomend__header}>
+				<h1 className={classes.recomend__title}>FEED MARKET рекомендует</h1>
+				<Button factor="yellow" size="s" className={classes.recomend__btn}>
+					Перейти в рекомендации
+				</Button>
+			</div>
+			<div className={classes.recomend__content}>
+				{/*{ad && <div className={classes.recomend__content_ad}>*/}
+				{/*	<Image/>*/}
+				{/*</div>}*/}
+				<div className={classes.recomend__content_card}>
+					{cards.map((card, i) => {
+						if (i < 4) {
+							return (
+								<Card
+									key={i}
+									reference={`/catalog/${card.id}`}
+									img={card.image}
+									title={card.title}
+									animal={card.form}
+									percentage={card.inputPercentage}
+									weight={card.weight}
+									price={card.price}
+									click={event => {
+										event.preventDefault();
+										alert('TECT');
+									}}
+								/>
+							);
+						}
+						return null;
+					})}
+				</div>
+			</div>
+		</div>
+	);
+};
 export default Recomend;
