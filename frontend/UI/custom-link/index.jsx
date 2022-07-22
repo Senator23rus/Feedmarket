a144ca1d9131fb6fb7518ff99560689249ce4359
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-07-22 11:29:04 +0300

    CHORE: FIX UI components

--- frontend/UI/custom-link/index.jsx
+++ frontend/UI/custom-link/index.jsx
@@ -4,15 +4,14 @@ import { useRouter } from 'next/router';
 /**
  * @description Кастомная ссылка для оборачивания Link от Next'а
  *
- * @param {string} href - путь на который должна ввести ссылка
- * @param {any} children - текст ссылки
- * @param {string} className - свой класс ссылки, заменит существующий
- * @param {string} active - класс для активной ссылки
+ * @param {import('next/link').LinkProps} props
+ * @property {string} [props.active]
  * @returns {JSX.Element}
  * @constructor
  */
-const CustomLink = ({ href, className, active, children }) => {
+const CustomLink = props => {
 	const { pathname } = useRouter();
+	const { href, className, active, children } = props;
 	if (pathname === href) {
 		return (
 			<span className={`${className || 'app-link'} ${active || ''}`}>{children}</span>
