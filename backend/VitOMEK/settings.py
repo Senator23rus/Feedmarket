0fc3d36ce1bfd154dd6293071a03b8fb3faf2541
senator23 <senator295@gmail.com>
2022-07-23 21:28:35 +0300

    ugettext_lazy

--- backend/VitOMEK/settings.py
+++ backend/VitOMEK/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = int(os.environ.get("DEBUG", default=1))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
-#ALLOWED_HOSTS = ['feed-market.ru', '151.248.114.18', 'localhost', '127.0.0.1']
+# ALLOWED_HOSTS = ['feed-market.ru', '151.248.114.18', 'localhost', '127.0.0.1']
 
 # Application definition
 
