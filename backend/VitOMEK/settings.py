db32b9dc3a039c8d2366dcc240b9fe4b8ee53a72
senator23 <senator295@gmail.com>
2022-07-23 23:00:02 +0300

    djoser & JWT token

--- backend/VitOMEK/settings.py
+++ backend/VitOMEK/settings.py
@@ -137,9 +137,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
