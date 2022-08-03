3ac2a5e7c6aadc62306d6aa48737f85c84a5646a
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-08-03 13:55:40 +0300

    CHORE: Test work home page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,10 @@ import Layout from 'components/common/layouts';
 import Recomend from 'components/pages/home/recomend/recomend';
 import cards from 'mock/n_cards.json';
 import NavBlock from 'components/pages/home/nav-block';
+import { useSelector } from 'react-redux';
+import { wrapper } from 'store';
+import { setTest } from 'store/slices/base-slice';
+import { useEffect } from 'react';
 
 /**
  *
@@ -11,10 +15,16 @@ import NavBlock from 'components/pages/home/nav-block';
  * @constructor
  */
 export default function Home({ cards }) {
+	const { test } = useSelector(state => state.base);
+	useEffect(() => {
+		console.log(test);
+	}, [test]);
+	console.log(test);
 	return (
 		<Layout>
 			<div className="home">
 				<NavBlock />
+				{test}
 				<Recomend cards={cards} />
 				<Recomend cards={cards} />
 				<Recomend cards={cards} />
@@ -22,9 +32,9 @@ export default function Home({ cards }) {
 		</Layout>
 	);
 }
-export async function getStaticProps() {
-	console.log(cards);
+export const getServerSideProps = wrapper.getServerSideProps(state => async () => {
+	state.dispatch(setTest());
 	return {
 		props: { cards },
 	};
-}
+});
