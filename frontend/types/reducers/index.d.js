d21ca5d720fe1d4957c35510bc48d69543fe8330
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-08-03 13:54:00 +0300

    FEAT: Add BaseReducer types

--- frontend/types/reducers/index.d.js
+++ frontend/types/reducers/index.d.js
@@ -2,4 +2,5 @@
  * @description Глобальный store
  * @typedef {Object} StateApp
  * @property {CatalogReducer} catalog - редьюсер управлением состояния счетчика
+ * @property {CatalogReducer} base - редьюсер управлением состояния счетчика
  */
