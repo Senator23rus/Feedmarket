8e1e655495c8466823e43080bfe64513235cf8f3
BeFunXdev <105725119+BeFunXdev@users.noreply.github.com>
2022-08-19 04:05:12 +0300

    completion Filter component

--- frontend/components/common/filters/filterBlock/index.js
+++ frontend/components/common/filters/filterBlock/index.js
@@ -1,128 +1,156 @@
 import classes from './filterBlock.module.scss';
-import Checkbox from "UI/checkbox";
-import {cloneElement, useEffect, useRef, useState} from "react";
-import classNames from "classnames";
-import Input from "UI/Input/input";
+import Checkbox from 'UI/checkbox';
+import { useEffect, useRef, useState } from 'react';
+import classNames from 'classnames';
+import Input from 'UI/Input/input';
 
-const FilterBlock = ({state, changeBrand, filterName, title, isСollapsible, isSearch}) => {
-    let [collapsed, setCollapsed] = useState(true);
-    let [search, setSearch] = useState('');
-    let [content, setContent] = useState([]);
-    let itemWrap = useRef();
-    let isLoadState = useRef(false);
-    let isLoad = useRef(true);
-    let noSearch = useRef(false);
+/**
+ * @param {React.ReactElement
+ * & {
+ * [state]:object[],
+ * [changeBrand]:Function,
+ * [filterName]:string,
+ * [title]:string,
+ * [isСollapsible]:boolean,
+ * isSearch:boolean}} props
+ * @constructor
+ */
 
-    const lengthCalc = (iter) => {
-        let len = 0;
-        for (let i = 0; i < iter; i++) {
-            len += itemWrap.current.children[i].clientHeight + 8;
-        }
-        itemWrap.current.style.setProperty('height',
-            len + 'px');
-    }
+const FilterBlock = ({
+	state,
+	changeBrand,
+	filterName,
+	title,
+	isСollapsible,
+	isSearch,
+}) => {
+	let [collapsed, setCollapsed] = useState(true);
+	let [search, setSearch] = useState('');
+	let [content, setContent] = useState([]);
+	let itemWrap = useRef();
+	let isLoad = useRef(true);
+	let noSearch = useRef(false);
 
-    const heightHandler = () => {
-        if (isСollapsible && itemWrap.current.children.length > 5) {
-            lengthCalc(5);
-            isLoad.current = false;
-        } else {
-            if (noSearch.current && itemWrap.current.children.length >= 10) {
-                let len = 0;
-                for (let i = 0; i < 10; i++) {
-                    len += itemWrap.current.children[i].clientHeight + 8;
-                }
-                itemWrap.current.style.setProperty('height',
-                    len + 'px');
-                noSearch.current = false;
-            } else {
-                lengthCalc(itemWrap.current.children.length);
-            }
-        }
-        if (content.length !== 0) {
-            isLoadState.current = true;
-        }
-    }
+	const lengthCalc = itemCount => {
+		let len = 0;
+		for (let i = 0; i < itemCount; i++) {
+			len += itemWrap.current.children[i]?.clientHeight + 8;
+		}
+		itemWrap.current.style.setProperty('height', len + 'px');
+	};
 
-    const onClickHandler = (e) => {
-        if (collapsed) {
-            if (isSearch && itemWrap.current.children.length >= 10) {
-                let len = 0;
-                for (let i = 0; i < 10; i++) {
-                    len += itemWrap.current.children[i].clientHeight + 8;
-                }
-                itemWrap.current.style.setProperty('height',
-                    len + 'px');
-            } else {
-                itemWrap.current.style.setProperty('height',
-                    '');
-            }
-            setCollapsed(false);
-        } else {
-            lengthCalc(itemWrap.current.children.length < 5 ? itemWrap.current.children.length : 5);
-            setCollapsed(true);
-        }
-    }
+	const heightHandler = () => {
+		if (collapsed && itemWrap.current.children.length > 5) {
+			lengthCalc(5);
+			isLoad.current = false;
+		} else {
+			if (
+				(noSearch.current && itemWrap.current.children.length >= 10) ||
+				(!noSearch.current && itemWrap.current.children.length >= 10)
+			) {
+				lengthCalc(10);
+			} else {
+				lengthCalc(itemWrap.current.children.length);
+			}
+		}
+	};
 
-    const onInputHandler = (e) => {
-        console.log(e.target.value);
-        if (e.target.value) {
-            let count = 0;
-            setContent(state.map(item => {
-                let checkboxClass = classes.hide;
-                console.log(item)
-                if (item.name.toLowerCase().includes(e.target.value.toLowerCase())) {
-                    count++;
-                    checkboxClass = '';
-                    return  <div key={item.id} className={checkboxClass}>
-                        <Checkbox
-                            onClick={() => changeBrand(item, index, filterName)}
-                            groupChecked={false}
-                            checked={item.active}>
-                            {item.name}
-                        </Checkbox>
-                    </div>
-                }
-            }));
-            lengthCalc(count > 5 ? 5 : count);
-        } else {
-            setContent([])
-            noSearch.current = true;
-        }
-        heightHandler();
-        setSearch(e.target.value);
-    }
+	const filter = text => {
+		setContent(
+			state.map((item, index) => {
+				if (item.name.toLowerCase().includes(text.toLowerCase())) {
+					return (
+						<div key={item.id}>
+							<Checkbox
+								onClick={() => changeBrand(item, index, filterName)}
+								groupChecked={false}
+								checked={item.active}>
+								{item.name}
+							</Checkbox>
+						</div>
+					);
+				}
+			})
+		);
+	};
 
-    useEffect(() => {
-        if (isLoad.current) {
-            heightHandler();
-        }
-    }, [content])
+	const onClickHandler = e => {
+		if (collapsed) {
+			if (isSearch && itemWrap.current.children.length >= 10) {
+				lengthCalc(10);
+			} else {
+				itemWrap.current.style.setProperty('height', '');
+			}
+			setCollapsed(false);
+		} else {
+			lengthCalc(
+				itemWrap.current.children.length < 5 ? itemWrap.current.children.length : 5
+			);
+			setCollapsed(true);
+		}
+	};
 
-  return(
-      <div className={'filter-block'}>
-          <p className={'title'}>{title}</p>
-          {!(!collapsed && isSearch) || <Input value={search} onInput={onInputHandler}
-                                               placeholder={'Поиск'} size={'medium'}
-                                               variant={'outlined'} searchIcon/>}
-          <div className={classNames('items', classes.filterBlock, !(!collapsed && isSearch) || classes.search)} ref={itemWrap}>
-              {content?.length !== 0 ? content : state.map((_, index) => {
-                  return (
-                      <div key={_.id}>
-                          <Checkbox
-                              onClick={() => changeBrand(_, index, filterName)}
-                              groupChecked={false}
-                              checked={_.active}>
-                              {_.name}
-                          </Checkbox>
-                      </div>
-                  );
-              })}
-          </div>
-          {!isСollapsible || (<button onClick={onClickHandler} className={classes.button}>
-              {collapsed ? 'Посмотреть все' : 'Свернуть'}</button>)}
-      </div>
-  );
-}
+	const onInputHandler = e => {
+		if (e.target.value) {
+			filter(e.target.value);
+			noSearch.current = false;
+		} else {
+			setContent([]);
+			noSearch.current = true;
+		}
+		setSearch(e.target.value);
+	};
+
+	useEffect(() => {
+		heightHandler();
+	}, [content]);
+
+	useEffect(() => {
+		if (!isLoad.current) filter(search);
+	}, [state]);
+
+	return (
+		<div className={'filter-block'}>
+			<p className={'title'}>{title}</p>
+			{!(!collapsed && isSearch) || (
+				<Input
+					value={search}
+					onInput={onInputHandler}
+					placeholder={'Поиск'}
+					size={'medium'}
+					variant={'outlined'}
+					searchIcon
+				/>
+			)}
+			<div
+				className={classNames(
+					'items',
+					classes.filterBlock,
+					!(!collapsed && isSearch) || classes.search
+				)}
+				ref={itemWrap}>
+				{content?.length !== 0
+					? content
+					: state.map((_, index) => {
+							return (
+								<div key={_.id}>
+									<Checkbox
+										onClick={() => changeBrand(_, index, filterName)}
+										groupChecked={false}
+										checked={_.active}>
+										{_.name}
+									</Checkbox>
+								</div>
+							);
+					  })}
+			</div>
+			{!isСollapsible || (
+				<button onClick={onClickHandler} className={classes.button}>
+					{collapsed ? 'Посмотреть все' : 'Свернуть'}
+				</button>
+			)}
+		</div>
+	);
+};
 
 export default FilterBlock;
--- frontend/components/common/filters/index.js
+++ frontend/components/common/filters/index.js
@@ -1,230 +1,239 @@
-import SliderLine from "UI/sliderLine";
-import ChipsButton from "UI/chipsButton";
-import {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import {useSelector} from "react-redux";
+import SliderLine from 'UI/sliderLine';
+import ChipsButton from 'UI/chipsButton';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import classes from './filters.module.scss';
-import FilterBlock from "components/common/filters/filterBlock";
-import Switch from "UI/switch";
-import classNames from "classnames";
+import FilterBlock from 'components/common/filters/filterBlock';
+import Switch from 'UI/switch';
+import classNames from 'classnames';
 
-const Filters = ({ state, setState}) => {
+/**
+ * @param {React.ReactElement
+ * & {
+ * [state]:object,
+ * [setState]:Function}} props
+ * @constructor
+ */
 
-    const { query } = useRouter();
+const Filters = ({ state, setState }) => {
+	const { query } = useRouter();
 
-    const [priceFilter, setPriceFilter] = useState(state.price);
-    const [weightFilter, setWeightFilter] = useState(state.weight);
-    const [priceFilterMM, setPriceFilterMM] = useState(state.price);
-    const [weightFilterMM, setWeightFilterMM] = useState(state.weight);
-    const [isCatch, setIsCatch] = useState(false);
+	const [priceFilter, setPriceFilter] = useState(state.price);
+	const [weightFilter, setWeightFilter] = useState(state.weight);
+	const [priceFilterMM] = useState(state.price);
+	const [weightFilterMM] = useState(state.weight);
+	const [isCatch, setIsCatch] = useState(false);
 
-    useEffect(() => {
-        if (!isCatch) {
-            setState(prevState => {return {...prevState, price: priceFilter, weight: weightFilter}});
-        }
-    }, [priceFilter, weightFilter]);
+	useEffect(() => {
+		if (!isCatch) {
+			setState(prevState => {
+				return { ...prevState, price: priceFilter, weight: weightFilter };
+			});
+		}
+	}, [priceFilter, weightFilter]);
 
-    const onMouseDownHandler = (e) => {
-        setIsCatch(true);
-    }
+	const onMouseDownHandler = e => {
+		setIsCatch(true);
+	};
 
-    const onMouseUpHandler = (e) => {
-        setIsCatch(true);
-        setState(prevState => {return {...prevState, price: priceFilter, weight: weightFilter}});
-    }
+	const onMouseUpHandler = e => {
+		setIsCatch(true);
+		setState(prevState => {
+			return { ...prevState, price: priceFilter, weight: weightFilter };
+		});
+	};
 
-    const [chips, setChips] = useState([]);
+	const [chips, setChips] = useState([]);
 
-    const changeBrand = (itemBrand, index, filterName) => {
-        console.log(itemBrand.id)
-        const brand = JSON.parse(JSON.stringify(state[filterName]));
-        brand[index] = { ...itemBrand, active: !itemBrand.active };
-        setState(prevState => ({ ...prevState, [filterName]: brand }));
-    };
+	const changeBrand = (itemBrand, index, filterName) => {
+		const brand = JSON.parse(JSON.stringify(state[filterName]));
+		brand[index] = { ...itemBrand, active: !itemBrand.active };
+		setState(prevState => ({ ...prevState, [filterName]: brand }));
+	};
 
-    const catalogState = useSelector(/**@param {StateApp} state*/ state => state.catalog);
+	const catalogState = useSelector(/**@param {StateApp} state*/ state => state.catalog);
 
-    useEffect(() => {
-        if (query.category && catalogState[query.category]) {
-            setState(prevState => ({
-                ...prevState,
-                types: catalogState[query.category].types.map(_ => ({ ..._, active: false })),
-            }));
-        } else {
-            setState(prevState => ({
-                ...prevState,
-                types: catalogState.pig.types.map(_ => ({ ..._, active: false })),
-            }));
-        }
-        //eslint-disable-next-line
-    }, [query]);
-    const changeActiveType = index => {
-        const newTypes = JSON.parse(JSON.stringify(state.types));
-        newTypes[index] = { ...newTypes[index], active: !newTypes[index].active };
-        setState(prevState => ({ ...prevState, types: newTypes }));
-    };
+	useEffect(() => {
+		if (query.category && catalogState[query.category]) {
+			setState(prevState => ({
+				...prevState,
+				types: catalogState[query.category].types.map(_ => ({ ..._, active: false })),
+			}));
+		} else {
+			setState(prevState => ({
+				...prevState,
+				types: catalogState.pig.types.map(_ => ({ ..._, active: false })),
+			}));
+		}
+	}, [query]);
+	const changeActiveType = index => {
+		const newTypes = JSON.parse(JSON.stringify(state.types));
+		newTypes[index] = { ...newTypes[index], active: !newTypes[index].active };
+		setState(prevState => ({ ...prevState, types: newTypes }));
+	};
 
-    const deleteChips = item => {
-        const newFilters = JSON.parse(JSON.stringify(state));
-        for (const filtersKey in newFilters) {
-            if (newFilters.hasOwnProperty(filtersKey)) {
-                const temp = newFilters[filtersKey].find(_ => _.name === item.name);
-                if (temp) {
-                    newFilters[filtersKey] = newFilters[filtersKey].map(_ =>
-                        _.id === temp.id ? { ..._, active: false } : { ..._ }
-                    );
-                }
-            }
-        }
-        setState(newFilters);
-    };
+	const deleteChips = item => {
+		const newFilters = JSON.parse(JSON.stringify(state));
+		for (const filtersKey in newFilters) {
+			if (newFilters.hasOwnProperty(filtersKey)) {
+				const temp = newFilters[filtersKey].find(_ => _.name === item.name);
+				if (temp) {
+					newFilters[filtersKey] = newFilters[filtersKey].map(_ =>
+						_.id === temp.id ? { ..._, active: false } : { ..._ }
+					);
+				}
+			}
+		}
+		setState(newFilters);
+	};
 
-    const clearAllFilters = () => {
-        const newFilters = JSON.parse(JSON.stringify(state));
-        for (const filtersKey in newFilters) {
-            if (filtersKey === 'brand' || filtersKey === 'types' ||
-                filtersKey === 'typeOfAnimal' || filtersKey === 'seller' ||
-                filtersKey === 'ageOfAnimal' || filtersKey === 'ageOfAnimal'
-                || filtersKey === 'countryOfOrigin') {
-                if (newFilters.hasOwnProperty(filtersKey)) {
-                    newFilters[filtersKey] = newFilters[filtersKey].map(_ => ({
-                        ..._,
-                        active: false,
-                    }));
-                }
-            }
-        }
-        setState(newFilters);
-    };
+	const clearAllFilters = () => {
+		const newFilters = JSON.parse(JSON.stringify(state));
+		for (const filtersKey in newFilters) {
+			if (filtersKey !== 'sale' && filtersKey !== 'weight' && filtersKey !== 'price') {
+				if (newFilters.hasOwnProperty(filtersKey)) {
+					newFilters[filtersKey] = newFilters[filtersKey].map(_ => ({
+						..._,
+						active: false,
+					}));
+				}
+			}
+		}
+		setState(newFilters);
+	};
 
-    useEffect(() => {
-        const arr = [];
-        for (const filtersKey in state) {
-            if (filtersKey === 'brand' || filtersKey === 'types' ||
-                filtersKey === 'typeOfAnimal' || filtersKey === 'seller' ||
-                filtersKey === 'ageOfAnimal' || filtersKey === 'ageOfAnimal'
-                || filtersKey === 'countryOfOrigin') {
-                const temp = state[filtersKey].filter(_ => _.active);
-                arr.push(temp);
-            }
-        }
-        if (arr.length) {
-            setChips(arr.flat());
-        }
-        //eslint-disable-next-line
-    }, [state.brand, state.types, state.typeOfAnimal,
-        state.seller, state.ageOfAnimal, state.countryOfOrigin]);
+	useEffect(() => {
+		const arr = [];
+		for (const filtersKey in state) {
+			if (filtersKey !== 'sale' && filtersKey !== 'weight' && filtersKey !== 'price') {
+				const temp = state[filtersKey].filter(_ => _.active);
+				arr.push(temp);
+			}
+		}
+		if (arr.length) {
+			setChips(arr.flat());
+		}
+	}, [
+		state.brand,
+		state.types,
+		state.typeOfAnimal,
+		state.seller,
+		state.ageOfAnimal,
+		state.countryOfOrigin,
+	]);
 
-  return(
-      <div className="catalog-page__wrapper">
-          <div className="catalog-page__wrapper-filter">
-              <div className="filter-block">
-                  <p className="title">Тип</p>
-                  <div className={'items'}>
-                      {state.types.map((_, index) => (
-                          <span
-                              onClick={() => changeActiveType(index)}
-                              className={`link ${_.active ? classes.activeLink : ''}`}
-                              key={index}>
-										{_.name}
-									</span>
-                      ))}
-                  </div>
-              </div>
-              <FilterBlock title={'Вид животного'} state={state.typeOfAnimal} filterName={'typeOfAnimal'} changeBrand={changeBrand} isСollapsible/>
-              <FilterBlock title={'Возраст животного'} filterName={'typeOfAnimal'} state={state.ageOfAnimal} changeBrand={changeBrand}/>
-              <FilterBlock state={state.countryOfOrigin} title={'Страна производитель'} filterName={'countryOfOrigin'}
-                           changeBrand={changeBrand}/>
-              <FilterBlock title={'Бренд'} filterName={'brand'} state={state.brand} changeBrand={changeBrand}
-               isСollapsible isSearch/>
-              <div className="filter-block">
-                  <p className="title">Вес, кг</p>
-                  <SliderLine
-                      values={weightFilter}
-                      setValue={setWeightFilter}
-                      max={weightFilterMM[1]}
-                      min={weightFilterMM[0]}
-                  />
-              </div>
-              <div className="filter-block">
-                  <p className="title">цена, руб</p>
-                  <SliderLine
-                      onMouseDown={onMouseDownHandler}
-                      onMouseUp={onMouseUpHandler}
-                      values={priceFilter}
-                      setValue={setPriceFilter}
-                      max={priceFilterMM[1]}
-                      min={priceFilterMM[0]}
-                  />
-              </div>
-              <div className={classNames('filter-block', classes.saleWrapper)}>
-                  <p className={classes.sale}>Товары со скидкой</p>
-                  <Switch checked={state.sale} onChange={() => setState(prevState => {return{...prevState,
-                      sale: !state.sale}})} />
-              </div>
-              <FilterBlock title={'Продавец'} filterName={'seller'} state={state.seller} changeBrand={changeBrand}
-              isСollapsible isSearch/>
-          </div>
-          <div className="catalog-page__wrapper-content catalog-page-wrap">
-              <div className="catalog-page-wrap__wrapper-cards">
-                  <div className="catalog-page-wrap__filte  rs_card">
-                      <div className="chips">
-                          {!!chips.length && (
-                              <>
-                                  {chips.map((_, index) => (
-                                      <ChipsButton
-                                          key={index}
-                                          onDelete={() => deleteChips(_)}
-                                          active={_.active}>
-                                          {_.name}
-                                      </ChipsButton>
-                                  ))}
-                                  <ChipsButton
-                                      onDelete={() => {}}
-                                      onClick={clearAllFilters}
-                                      active={false}>
-                                      Очистить
-                                  </ChipsButton>
-                              </>
-                          )}
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  );
-}
+	return (
+		<div className="catalog-page__wrapper">
+			<div className="catalog-page__wrapper-filter">
+				<div className="filter-block">
+					<p className="title">Тип</p>
+					<div className={'items'}>
+						{state.types.map((_, index) => (
+							<span
+								onClick={() => changeActiveType(index)}
+								className={`link ${_.active ? classes.activeLink : ''}`}
+								key={index}>
+								{_.name}
+							</span>
+						))}
+					</div>
+				</div>
+				<FilterBlock
+					title={'Вид животного'}
+					state={state.typeOfAnimal}
+					filterName={'typeOfAnimal'}
+					changeBrand={changeBrand}
+					isСollapsible
+				/>
+				<FilterBlock
+					title={'Возраст животного'}
+					filterName={'ageOfAnimal'}
+					state={state.ageOfAnimal}
+					changeBrand={changeBrand}
+				/>
+				<FilterBlock
+					state={state.countryOfOrigin}
+					title={'Страна производитель'}
+					filterName={'countryOfOrigin'}
+					changeBrand={changeBrand}
+				/>
+				<FilterBlock
+					title={'Бренд'}
+					filterName={'brand'}
+					state={state.brand}
+					changeBrand={changeBrand}
+					isСollapsible
+					isSearch
+				/>
+				<div className="filter-block">
+					<p className="title">Вес, кг</p>
+					<SliderLine
+						values={weightFilter}
+						setValue={setWeightFilter}
+						max={weightFilterMM[1]}
+						min={weightFilterMM[0]}
+					/>
+				</div>
+				<div className="filter-block">
+					<p className="title">цена, руб</p>
+					<SliderLine
+						onMouseDown={onMouseDownHandler}
+						onMouseUp={onMouseUpHandler}
+						values={priceFilter}
+						setValue={setPriceFilter}
+						max={priceFilterMM[1]}
+						min={priceFilterMM[0]}
+					/>
+				</div>
+				<div className={classNames('filter-block', classes.saleWrapper)}>
+					<p className={classes.sale}>Товары со скидкой</p>
+					<Switch
+						checked={state.sale}
+						onChange={() =>
+							setState(prevState => {
+								return { ...prevState, sale: !state.sale };
+							})
+						}
+					/>
+				</div>
+				<FilterBlock
+					title={'Продавец'}
+					filterName={'seller'}
+					state={state.seller}
+					changeBrand={changeBrand}
+					isСollapsible
+					isSearch
+				/>
+			</div>
+			<div className="catalog-page__wrapper-content catalog-page-wrap">
+				<div className="catalog-page-wrap__wrapper-cards">
+					<div className="catalog-page-wrap__filte  rs_card">
+						<div className="chips">
+							{!!chips.length && (
+								<>
+									{chips.map((_, index) => (
+										<ChipsButton
+											key={index}
+											onDelete={() => deleteChips(_)}
+											active={_.active}>
+											{_.name}
+										</ChipsButton>
+									))}
+									<ChipsButton
+										onDelete={() => {}}
+										onClick={clearAllFilters}
+										active={false}>
+										Очистить
+									</ChipsButton>
+								</>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default Filters;
-
-//<div className="filter-block">
-//                   <p className="title">Категория</p>
-//                   <div className={'items'}>
-//                       <Link href={{ pathname: '/catalog', query: { category: 'poetry' } }}>
-//                           <a
-//                               className={`link ${
-//                                   query.category === 'poetry' ? classes.activeLink : ''
-//                               }`}>
-//                               Птицеводство
-//                           </a>
-//                       </Link>
-//                       <Link href={{ pathname: '/catalog', query: { category: 'pig' } }}>
-//                           <a className={`link ${query.category === 'pig' ? classes.activeLink : ''}`}>
-//                               Свиноводство
-//                           </a>
-//                       </Link>
-//                       <Link href={{ pathname: '/catalog', query: { category: 'cattle' } }}>
-//                           <a
-//                               className={`link ${
-//                                   query.category === 'cattle' ? classes.activeLink : ''
-//                               }`}>
-//                               КРС
-//                           </a>
-//                       </Link>
-//                       <Link href={{ pathname: '/catalog', query: { category: 'raw' } }}>
-//                           <a className={`link ${query.category === 'raw' ? classes.activeLink : ''}`}>
-//                               Сырье
-//                           </a>
-//                       </Link>
-//                   </div>
-//               </div>
