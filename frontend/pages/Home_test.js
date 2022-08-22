845a279381c1617cffc5bde705a8f4e5a26b5ab9
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2022-08-22 15:04:16 +0300

    FIX

--- frontend/pages/Home_test.js
+++ frontend/pages/Home_test.js
@@ -1,45 +1,45 @@
-import cards from '/mock/cards.json';
-import Ad from 'UI/ads/ad';
-import Layout from 'components/common/layouts';
-import CardGrid from 'components/common/cards_grid/cards_grid';
-import Recomend from 'components/pages/home/recomend/recomend';
-
-const Test = ({ cards }) => {
-	console.log(cards);
-	return (
-		<Layout>
-			<div className="home">
-				{/*<Ad img={cards[0].imgAd} reference={cards[0].reference} type={cards[0].large}/>*/}
-				<div className="card__top">
-					<Recomend />
-					<CardGrid type="row" cards={cards} />
-				</div>
-
-				<div className="card__middle">
-					<Recomend />
-					<div className="card__block__wrapper">
-						<Ad
-							img={cards[0].imgAd}
-							reference={cards[0].reference}
-							type={cards[0].vertical}
-						/>
-						<CardGrid type="block" cards={cards} />
-					</div>
-				</div>
-
-				<div className="card__bottom">
-					<Recomend />
-					<Ad img={cards[0].imgAd} reference={cards[0].reference} type={cards[0].large} />
-					<CardGrid type="row" cards={cards} />
-				</div>
-			</div>
-		</Layout>
-	);
-};
-export async function getStaticProps() {
-	console.log(cards);
-	return {
-		props: cards,
-	};
-}
-export default Test;
+// import cards from '/mock/cards.json';
+// import Ad from 'UI/ads/ad';
+// import Layout from 'components/common/layouts';
+// import CardGrid from 'components/common/cards_grid/cards_grid';
+// import Recomend from 'components/pages/home/recomend/recomend';
+//
+// const Test = ({ cards }) => {
+// 	console.log(cards);
+// 	return (
+// 		<Layout>
+// 			<div className="home">
+// 				{/*<Ad img={cards[0].imgAd} reference={cards[0].reference} type={cards[0].large}/>*/}
+// 				<div className="card__top">
+// 					<Recomend />
+// 					<CardGrid type="row" cards={cards} />
+// 				</div>
+//
+// 				<div className="card__middle">
+// 					<Recomend />
+// 					<div className="card__block__wrapper">
+// 						<Ad
+// 							img={cards[0].imgAd}
+// 							reference={cards[0].reference}
+// 							type={cards[0].vertical}
+// 						/>
+// 						<CardGrid type="block" cards={cards} />
+// 					</div>
+// 				</div>
+//
+// 				<div className="card__bottom">
+// 					<Recomend />
+// 					<Ad img={cards[0].imgAd} reference={cards[0].reference} type={cards[0].large} />
+// 					<CardGrid type="row" cards={cards} />
+// 				</div>
+// 			</div>
+// 		</Layout>
+// 	);
+// };
+// export async function getStaticProps() {
+// 	console.log(cards);
+// 	return {
+// 		props: cards,
+// 	};
+// }
+// export default Test;
